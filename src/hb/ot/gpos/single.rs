9677cfe6d54474967b49689f9f7178993e73cbaa
Chad Brokaw <cbrokaw@gmail.com>
2025-08-15 17:20:01 -0400

    Unify on new value record application (#169)
    Updated treatment for pairpos1 and singlepos1/2 to match the work for pairpos2 in #167

--- src/hb/ot/gpos/single.rs
+++ src/hb/ot/gpos/single.rs
@@ -1,4 +1,3 @@
-use super::ValueResolver;
 use crate::hb::ot_layout_gsubgpos::Apply;
 use crate::hb::ot_layout_gsubgpos::OT::hb_ot_apply_context_t;
 use read_fonts::tables::gpos::{SinglePosFormat1, SinglePosFormat2};
@@ -7,12 +6,9 @@ impl Apply for SinglePosFormat1<'_> {
     fn apply(&self, ctx: &mut hb_ot_apply_context_t) -> Option<()> {
         let glyph = ctx.buffer.cur(0).as_glyph();
         self.coverage().ok()?.get(glyph)?;
-        let record = self.value_record();
-        let value = ValueResolver {
-            record,
-            data: self.offset_data(),
-        };
-        value.apply(ctx, ctx.buffer.idx);
+        let format = self.value_format();
+        let offset = self.shape().value_record_byte_range().start;
+        super::apply_value(ctx, ctx.buffer.idx, &self.offset_data(), offset, format);
         ctx.buffer.idx += 1;
         Some(())
     }
@@ -22,12 +18,10 @@ impl Apply for SinglePosFormat2<'_> {
     fn apply(&self, ctx: &mut hb_ot_apply_context_t) -> Option<()> {
         let glyph = ctx.buffer.cur(0).as_glyph();
         let index = self.coverage().ok()?.get(glyph)? as usize;
-        let record = self.value_records().get(index).ok()?;
-        let value = ValueResolver {
-            record,
-            data: self.offset_data(),
-        };
-        value.apply(ctx, ctx.buffer.idx);
+        let format = self.value_format();
+        let offset =
+            self.shape().value_records_byte_range().start + (format.record_byte_len() * index);
+        super::apply_value(ctx, ctx.buffer.idx, &self.offset_data(), offset, format);
         ctx.buffer.idx += 1;
         Some(())
     }
