cab22fc09847c122ffc19fcc5070c0f2152abc17
Behdad Esfahbod <behdad@behdad.org>
2025-08-26 16:04:58 -0600

    Use a digest for no_std U32Set

--- src/lib.rs
+++ src/lib.rs
@@ -14,29 +14,8 @@ mod hb;
 
 #[cfg(feature = "std")]
 pub(crate) type U32Set = read_fonts::collections::int_set::U32Set;
-
 #[cfg(not(feature = "std"))]
-pub(crate) struct DummyU32Set;
-
-#[cfg(not(feature = "std"))]
-impl DummyU32Set {
-    pub(crate) fn default() -> Self {
-        Self
-    }
-    pub(crate) fn insert(&mut self, _value: u32) {}
-    pub(crate) fn insert_range(&mut self, _range: core::ops::RangeInclusive<u32>) {}
-    pub(crate) fn clear(&mut self) {}
-    pub(crate) fn extend_unsorted<U: IntoIterator<Item = u32>>(&mut self, _iter: U) {}
-    pub(crate) fn contains(&self, _value: u32) -> bool {
-        true
-    }
-    pub(crate) fn intersects_set(&self, _other: &Self) -> bool {
-        true
-    }
-}
-
-#[cfg(not(feature = "std"))]
-pub(crate) type U32Set = DummyU32Set;
+pub(crate) type U32Set = crate::dummy_u32_set::DummyU32Set;
 
 pub use read_fonts::{types::Tag, FontRef};
 
